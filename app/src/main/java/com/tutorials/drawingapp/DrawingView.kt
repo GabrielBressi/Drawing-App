5a057c6d643fe4fb6f364a8013c85f848a6a0197
GabrielBressi <gabrielbressi18@gmail.com>
2023-01-31 16:13:27 -0300

    Added function for change behavior on different sizes of screen

--- app/src/main/java/com/tutorials/drawingapp/DrawingView.kt
+++ app/src/main/java/com/tutorials/drawingapp/DrawingView.kt
@@ -7,6 +7,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -38,7 +39,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        //mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -101,6 +102,13 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         return true
     }
 
+    fun setSizeForBrush(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
+
+
+    // An inner class for customer path with two params as colod and stroke
     internal inner class CustomPath(var color: Int, var brushTickness: Float) : Path() {
 
     }
