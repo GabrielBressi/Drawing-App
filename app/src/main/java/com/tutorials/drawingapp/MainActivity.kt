5f4981f0c08c125bdb46f65e96356953471d084d
GabrielBressi <gabrielbressi18@gmail.com>
2023-02-01 21:10:29 -0300

    implemented the gallery access, and the function to add pictures to background

--- app/src/main/java/com/tutorials/drawingapp/MainActivity.kt
+++ app/src/main/java/com/tutorials/drawingapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tutorials.drawingapp
 
+import android.app.AlertDialog
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,14 +8,50 @@ import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import android.Manifest
+import android.content.Intent
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
-
     private var mImageButtonCurrentPaint: ImageButton? = null
+
+    val openGalleryLaucher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result ->
+            if(result.resultCode == RESULT_OK && result.data!=null) {
+                val imageBackGround: ImageView = findViewById(R.id.iv_background)
+
+                imageBackGround.setImageURI(result.data?.data)
+            }
+        }
+
+    val requestPermission : ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            permissions ->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if(isGranted) {
+                    Toast.makeText(this, "Permission granted now you can read the storage files.", Toast.LENGTH_LONG).show()
+
+                    val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLaucher.launch(pickIntent)
+                }else {
+                    if(permissionName==Manifest.permission.READ_EXTERNAL_STORAGE){
+                        Toast.makeText(this, "Oops you just denied the permission.", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +70,11 @@ class MainActivity : AppCompatActivity() {
         val ibBrush : ImageButton = findViewById(R.id.ib_brush)
         ibBrush.setOnClickListener{
             showBrushSizeChooserDialog()
+        }
+
+        val ibGalerry : ImageButton = findViewById(R.id.ib_gallery)
+        ibGalerry.setOnClickListener {
+            requestStoragePermission()
         }
 
     }
@@ -78,5 +120,34 @@ class MainActivity : AppCompatActivity() {
             mImageButtonCurrentPaint = view
         }
     }
+
+    private fun requestStoragePermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+        ) {
+           showRationaleDialog("Drawing app", "Drawing app needs to Access Your External Storage")
+        }else {
+            requestPermission.launch(arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+                // TODO - Add writing external storage permission
+            ))
+        }
+    }
+
+    private fun showRationaleDialog(
+        title: String,
+        message: String
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
+
+
 
 }
